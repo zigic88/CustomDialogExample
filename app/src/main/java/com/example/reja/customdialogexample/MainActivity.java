c3b6148ac1b7833f8c1f2bfacc1baf0e6e73edbe
zigicgunz <shinhyegunz@gmail.com>
2016-08-24 21:23:02 +0700

    - Custom Dialog Example

---
+++ app/src/main/java/com/example/reja/customdialogexample/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.reja.customdialogexample;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnsave;
+    private TextInputLayout txtlayout, txtlayout2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtlayout = (TextInputLayout) findViewById(R.id.input_layout);
+        txtlayout2 = (TextInputLayout) findViewById(R.id.input_layout2);
+        btnsave = (Button) findViewById(R.id.btnsave);
+        btnsave.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                showInputDialog();
+            }
+        });
+    }
+
+    protected void showInputDialog() {
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        final View promptView = layoutInflater.inflate(R.layout.prompt, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
+
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // when press OK
+                        Toast.makeText(promptView.getContext(), "OK", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // when press cancel
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+}
